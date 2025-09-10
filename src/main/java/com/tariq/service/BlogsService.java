8c1ee433bf61a15d7ddf41561538dc3a66e5b35a
tar455 <tariqueansari455@gmail.com>
2025-09-10 11:27:29 +0530

    Tarique: added Jwt login

--- src/main/java/com/tariq/service/BlogsService.java
+++ src/main/java/com/tariq/service/BlogsService.java
@@ -3,9 +3,13 @@ package com.tariq.service;
 import com.tariq.exception.BusinessException;
 import com.tariq.model.UserDetails;
 
+import java.util.Map;
+
 public interface BlogsService {
 
     void getAllblogsFromDB();
 
     void register(UserDetails user) throws BusinessException;
+
+    Map<String,Object> login(UserDetails user) throws BusinessException;
 }
